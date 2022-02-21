00c13f21de18c5c7c38be0fead8b462d62857d6c
Fahad Lodhi <fahad.lodhi@astera.com>
2022-02-21 19:52:42 +0500

    Add project files.

---
+++ RunNotepad/CsvConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace RunNotepad
+{
+    internal class CsvConfiguration
+    {
+        private object invariantCulture;
+
+        public CsvConfiguration(object invariantCulture)
+        {
+            this.invariantCulture = invariantCulture;
+        }
+
+        public bool HasHeaderRecord { get; set; }
+    }
+}
---
+++ RunNotepad/Program.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunNotepad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WindowsDriver<WindowsElement> notepadSession;
+            AppiumOptions desiredCapabilities = new AppiumOptions();
+            desiredCapabilities.AddAdditionalCapability("app", @"C:\Program Files\Astera Software\Centerprise Data Integrator 8\Centerprise.exe");
+            try
+            {
+                notepadSession = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723/"), desiredCapabilities);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return;
+            }
+            if (notepadSession != null)
+            {
+                notepadSession.FindElementByName("OK").Click();
+                notepadSession.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                IEnumerable<AppiumWebElement> elementsOne = notepadSession.FindElementsByAccessibilityId("ToolBarAreaTop");
+                
+            }
+        }
+    }
+}
